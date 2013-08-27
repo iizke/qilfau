3702b05bd235db9ee994201c06df6a0c0d8aa313
rosemanit@gmail.com <rosemanit@gmail.com@f6dfe70b-6d3a-8d76-fdeb-758e3606b1dd>
2013-08-27 04:09:19 +0000

    rebuild release

--- netlib/Release/src/netsim/queue_babs.d
+++ netlib/Release/src/netsim/queue_babs.d
@@ -1,7 +1,7 @@
 src/netsim/queue_babs.d: ../src/netsim/queue_babs.c \
  /home/iizke/workspace/netlib/src/error.h \
  /home/iizke/workspace/netlib/src/list/linked_list.h \
- ../src/netsim/queues/burst_fifo.h ../src/netsim/queues/queue_man.h \
+ ../src/netsim/queues/burst_queue.h ../src/netsim/queues/queue_man.h \
  ../src/netsim/queues/packet.h /home/iizke/workspace/netlib/src/def.h \
  /home/iizke/workspace/netlib/src/list/linked_list.h \
  ../src/netsim/queues/measures.h \
@@ -23,7 +23,7 @@ src/netsim/queue_babs.d: ../src/netsim/queue_babs.c \
 
 /home/iizke/workspace/netlib/src/list/linked_list.h:
 
-../src/netsim/queues/burst_fifo.h:
+../src/netsim/queues/burst_queue.h:
 
 ../src/netsim/queues/queue_man.h:
 
--- netlib/Release/src/netsim/queues/burst_fifo.d
+++ netlib/Release/src/netsim/queues/burst_fifo.d
@@ -1,7 +1,7 @@
 src/netsim/queues/burst_fifo.d: ../src/netsim/queues/burst_fifo.c \
  /home/iizke/workspace/netlib/src/error.h \
  /home/iizke/workspace/netlib/src/list/linked_list.h \
- ../src/netsim/queues/burst_fifo.h ../src/netsim/queues/queue_man.h \
+ ../src/netsim/queues/burst_queue.h ../src/netsim/queues/queue_man.h \
  ../src/netsim/queues/packet.h /home/iizke/workspace/netlib/src/def.h \
  /home/iizke/workspace/netlib/src/list/linked_list.h \
  ../src/netsim/queues/measures.h \
@@ -11,7 +11,7 @@ src/netsim/queues/burst_fifo.d: ../src/netsim/queues/burst_fifo.c \
 
 /home/iizke/workspace/netlib/src/list/linked_list.h:
 
-../src/netsim/queues/burst_fifo.h:
+../src/netsim/queues/burst_queue.h:
 
 ../src/netsim/queues/queue_man.h:
 
